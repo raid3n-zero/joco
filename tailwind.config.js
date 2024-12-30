14322976c5e0952de8835891e15a1f6c9a1b5456
raid3n-zero <mehimehilan1111@gmail.com>
2024-12-30 21:27:32 +0530

    joco up

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+const { addIconSelectors } = require('@iconify/tailwind');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "serif"]
+      }
+    },
+  },
+  plugins: [
+    addIconSelectors(["solar", "basil"]),
+  ],
+}
